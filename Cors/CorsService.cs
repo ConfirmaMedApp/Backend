eaf76b8d4d2a131c81d717e6da89cc1318d8f28e
Leonardo De La Cruz <leonardo.estudios200314@gmail.com>
2026-09-05 01:15:25 -0500

    Se actualizaron los CORS para el backend

--- Cors/CorsService.cs
+++ Cors/CorsService.cs
@@ -9,7 +9,7 @@ public static class CorsService
             options.AddPolicy("Confirm_Med_Rule", policy =>
             {
                 policy
-                    .WithOrigins("http://localhost:3001", "http://localhost:3002", "https://confirmamedtest.netlify.app", "https://confirmamedtest.netlify.app/", "https://patientconfirmamedtest.netlify.app/", "https://patientconfirmamedtest.netlify.app")
+                    .WithOrigins("http://localhost:3001", "http://localhost:3002", "https://confirmamedtest.netlify.app", "https://confirmamedtest.netlify.app/", "https://patientconfirmamedtest.netlify.app/", "https://patientconfirmamedtest.netlify.app", "https://frontend-production-0683.up.railway.app", "frontendpatients-production.up.railway.app")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();

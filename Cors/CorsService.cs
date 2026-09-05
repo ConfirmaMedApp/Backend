f342eb2a58f707a387c30c5e960318a5de2aaef8
Leonardo De La Cruz <leonardo.estudios200314@gmail.com>
2026-09-05 01:20:23 -0500

    Probando los CORS para pacientes

--- Cors/CorsService.cs
+++ Cors/CorsService.cs
@@ -9,7 +9,12 @@ public static class CorsService
             options.AddPolicy("Confirm_Med_Rule", policy =>
             {
                 policy
-                    .WithOrigins("http://localhost:3001", "http://localhost:3002", "https://confirmamedtest.netlify.app", "https://confirmamedtest.netlify.app/", "https://patientconfirmamedtest.netlify.app/", "https://patientconfirmamedtest.netlify.app", "https://frontend-production-0683.up.railway.app", "frontendpatients-production.up.railway.app")
+                    .WithOrigins("http://localhost:3001", 
+                        "http://localhost:3002", 
+                        "https://frontend-production-0683.up.railway.app",
+                        "https://frontend-production-0683.up.railway.app/", 
+                        "https://frontendpatients-production.up.railway.app",
+                        "https://frontendpatients-production.up.railway.app/")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
